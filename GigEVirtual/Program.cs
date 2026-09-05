d15cfb29ce87f95237befe7620c0d07b19b364bf
cenfraGit <cenfra_ma@hotmail.com>
2026-09-05 06:57:53 -0600

    docs: show how to start a genie nano in the entry point

--- GigEVirtual/Program.cs
+++ GigEVirtual/Program.cs
@@ -14,5 +14,12 @@ internal class Program
         //GigECamera cam2 = new("192.168.100.138", serialNumber:"S0002", deviceName: "secondDevice");
         //await Task.WhenAll(cam1.Start(), cam2.Start());
         await cam1.Start();
+
+        // the genie nano needs the path to its own description, which is not in
+        // the repo. everything else about it comes out of that file.
+        //GenieNano nano = new("192.168.100.137",
+        //                     xmlPath: @"C:\Users\MiguelCeniceros\Documents\Repos\GigEVirtual\genie_nano.xml",
+        //                     imagePath: @"C:\Users\MiguelCeniceros\Pictures\EmulationBedford\CD_Diffuse+Photometric.jpg");
+        //await nano.Start();
     }
 }
